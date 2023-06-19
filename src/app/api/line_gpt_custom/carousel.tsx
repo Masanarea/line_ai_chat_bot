0164c3f31f8c6e04a13d1abcb903227007ebd873
Masanarea <93495976+Masanarea@users.noreply.github.com>
2023-06-19 11:35:53 +0900

    chat_gpt フォルダーをコピーする

---
+++ src/app/api/line_gpt_custom/carousel.tsx
@@ -0,0 +1,60 @@
+import { FlexMessage, FlexContainer } from '@line/bot-sdk'
+
+const createCarouselTemplate = (): FlexMessage => {
+    const flex: FlexContainer = {
+        type: 'carousel',
+        contents: [
+            {
+                type: 'bubble',
+                body: {
+                    type: 'box',
+                    layout: 'vertical',
+                    contents: [
+                        {
+                            type: 'text',
+                            text: 'Option 1',
+                        },
+                        {
+                            type: 'button',
+                            action: {
+                                type: 'postback',
+                                label: 'Choose',
+                                data: 'option=1',
+                            },
+                        },
+                    ],
+                },
+            },
+            {
+                type: 'bubble',
+                body: {
+                    type: 'box',
+                    layout: 'vertical',
+                    contents: [
+                        {
+                            type: 'text',
+                            text: 'Option 2',
+                        },
+                        {
+                            type: 'button',
+                            action: {
+                                type: 'postback',
+                                label: 'Choose',
+                                data: 'option=2',
+                            },
+                        },
+                    ],
+                },
+            },
+            // Add more bubbles if needed
+        ],
+    }
+
+    const message: FlexMessage = {
+        type: 'flex',
+        altText: 'This is a carousel template',
+        contents: flex,
+    }
+
+    return message
+}
